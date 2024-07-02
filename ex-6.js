d570a0cb3a693bf24f7317bb766a45a131b065f8
Min <minbcc161@gmail.com>
2024-07-02 20:54:08 +0700

    done

--- ex-6.js
+++ ex-6.js
@@ -1,5 +1,5 @@
 // Exercise #6
-let getJohnProfile = () => {
+let getJohnProfile = async () => {
   return new Promise(function (resolve) {
     setTimeout(
       () =>
@@ -13,7 +13,7 @@ let getJohnProfile = () => {
   });
 };
 
-let getJohnOrders = () => {
+let getJohnOrders = async() => {
   return new Promise(function (resolve) {
     setTimeout(
       () =>
@@ -32,4 +32,10 @@ let getJohnOrders = () => {
   });
 };
 
-// Start coding here
+async function asynchronousfunction(){
+  const JohnProfile = await getJohnProfile();
+  console.log(JohnProfile);
+  const JohnOrders = await getJohnOrders();
+  console.log(JohnOrders);
+}
+asynchronousfunction();
